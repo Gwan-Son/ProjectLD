54cc2918877d7797e90b7c3445fd834d5bb25723
Gwan-Son <id147369@naver.com>
2026-05-26 14:37:09 +0900

    Initial Commit

---
+++ Longdy/LongdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongdyApp.swift
+//  Longdy
+//
+//  Created by 심관혁 on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct LongdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
